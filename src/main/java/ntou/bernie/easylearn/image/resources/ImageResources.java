97d961340e28dc0bd44448c6cc2883e8b8c6c776
Bernie <b4456609@gmail.com>
2016-03-23 18:08:52 +0800

    add img

---
+++ src/main/java/ntou/bernie/easylearn/image/resources/ImageResources.java
@@ -0,0 +1,7 @@
+package ntou.bernie.easylearn.image.resources;
+
+/**
+ * Created by bernie on 2016/3/23.
+ */
+public class ImageResources {
+}
---
+++ src/main/java/ntou/bernie/easylearn/mobile/core/ImgItem.java
@@ -0,0 +1,43 @@
+package ntou.bernie.easylearn.mobile.core;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
+/**
+ * Created by bernie on 2016/3/23.
+ */
+@Entity
+public class ImgItem {
+    @Id
+    private ObjectId _id;
+    @JsonProperty
+    private String id;
+    @JsonProperty
+    private String deletehash;
+
+    public ImgItem() {
+    }
+
+    public ImgItem(String id, String deletehash) {
+        this.id = id;
+        this.deletehash = deletehash;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDeletehash() {
+        return deletehash;
+    }
+
+    public void setDeletehash(String deletehash) {
+        this.deletehash = deletehash;
+    }
+}
